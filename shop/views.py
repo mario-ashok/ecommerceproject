65f6fb04c7162dd3b601358006284e39ede6f3c3
mario-ashok <marioashoka@gmail.com>
2019-12-04 20:53:14 +0530

    Added Search Bar

--- shop/views.py
+++ shop/views.py
@@ -4,5 +4,11 @@ from .models import  Product
 
 def index(request):
 	products = Product.objects.all()
+
+	product_name = request.GET.get('item_name')
+	if product_name!='' and product_name is not None:
+		products=Product.objects.filter(title__icontains=product_name)
+
+
 	return render(request,"shop/index.html",{'products':products})
 
